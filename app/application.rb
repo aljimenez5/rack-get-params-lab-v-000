ed00ad9d4f64597f1d61ebc8ec1a45eff701ab2e
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 02:50:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,8 +27,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty"
       else 
-        @@cart.each do |item|
-          resp.write "#{item}\n"
+        @@cart.each {|item| resp.write "#{item}\n"}
         end
       end
     else
